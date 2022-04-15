e48c2948174090cd7a433514b0fd8c3f41caa6f2
mike-k-git <mikehsm@outlook.com>
2022-04-15 20:33:54 +0300

    5.14: Blog list tests, step2

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,43 +1,60 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event/'
 import Blog from './Blog'
 
-test("renders only blogs's title and author by default", () => {
-  const blog = {
-    title: 'TDD harms architecture',
-    url: 'http://www.u.arizona.edu/',
-    author: 'Michael Chan',
-    id: '1293179231',
-    likes: 0,
-    user: {
-      username: 'username',
-      name: 'name',
-      id: '123123976123',
-    },
-  }
-  const likes = jest.fn()
-  const removeBlog = jest.fn()
-  const currentUser = 'username'
+describe('<Blog />', () => {
+  let container, likes, removeBlog, currentUser
 
-  const { container } = render(
-    <Blog
-      blog={blog}
-      likes={likes}
-      removeBlog={removeBlog}
-      currentUser={currentUser}
-    />
-  )
+  beforeEach(() => {
+    const blog = {
+      title: 'TDD harms architecture',
+      url: 'http://www.u.arizona.edu/',
+      author: 'Michael Chan',
+      id: '1293179231',
+      likes: 0,
+      user: {
+        username: 'username',
+        name: 'name',
+        id: '123123976123',
+      },
+    }
+    likes = jest.fn()
+    removeBlog = jest.fn()
+    currentUser = 'username'
 
-  const titleSpan = container.querySelector('.blog-title')
-  expect(titleSpan).toBeDefined()
+    container = render(
+      <Blog
+        blog={blog}
+        likes={likes}
+        removeBlog={removeBlog}
+        currentUser={currentUser}
+      />
+    ).container
+  })
 
-  const authorSpan = container.querySelector('.blog-author')
-  expect(authorSpan).toBeDefined()
+  test("renders only blogs's title and author by default", () => {
+    const titleSpan = container.querySelector('.blog-title')
+    expect(titleSpan).toBeDefined()
 
-  const urlSpan = container.querySelector('.blog-url')
-  screen.debug(urlSpan)
-  expect(urlSpan).toBeNull()
+    const authorSpan = container.querySelector('.blog-author')
+    expect(authorSpan).toBeDefined()
 
-  const likesSpan = container.querySelector('.blog-likes')
-  expect(likesSpan).toBeNull()
+    const urlSpan = container.querySelector('.blog-url')
+    expect(urlSpan).toBeNull()
+
+    const likesSpan = container.querySelector('.blog-likes')
+    expect(likesSpan).toBeNull()
+  })
+
+  test("blog's url and number of likes are shown wthen the button clicked", async () => {
+    const button = screen.getByText('view')
+    await userEvent.click(button)
+
+    const urlSpan = container.querySelector('.blog-url')
+    expect(urlSpan).toBeDefined()
+
+    const likesSpan = container.querySelector('.blog-likes')
+    expect(likesSpan).toBeDefined()
+  })
 })
