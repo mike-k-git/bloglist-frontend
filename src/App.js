3c0055cb4f02ac0f6fa197f8eb288596ae9ac183
mike-k-git <mikehsm@outlook.com>
2022-04-15 03:33:51 +0300

    5.9: Blog list frontend, step9

--- src/App.js
+++ src/App.js
@@ -103,9 +103,11 @@ const App = () => {
         <Togglable ref={blogFormRef}>
           <BlogForm createBlog={createBlog} />
         </Togglable>
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} likes={() => increaseLikes(blog)} />
-        ))}
+        {blogs
+          .sort((a, b) => b.likes - a.likes)
+          .map((blog) => (
+            <Blog key={blog.id} blog={blog} likes={() => increaseLikes(blog)} />
+          ))}
       </div>
     )
   }
