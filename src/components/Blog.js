f7a9146d19d3559f0c067bb432c263cb1410b34b
mike-k-git <mikehsm@outlook.com>
2022-04-15 03:03:30 +0300

    5.7* Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [fullInfo, setFullInfo] = useState(false)
+
+  const toggleFullInfo = () => {
+    setFullInfo(!fullInfo)
+  }
+
+  const style = {
+    fontSize: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    border: 'solid',
+    marginTop: 5,
+    marginBottom: 5,
+    lineHeight: '1.5em',
+  }
+
+  if (fullInfo) {
+    return (
+      <div style={style}>
+        <div>
+          {blog.title} {blog.author}{' '}
+          <button onClick={toggleFullInfo}>hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={style}>
+      {blog.title} {blog.author} <button onClick={toggleFullInfo}>view</button>
+    </div>
+  )
+}
 
 export default Blog
