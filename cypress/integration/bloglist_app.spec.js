90021f69a872d2565791a8e90c38617f2f62c2f4
mike-k-git <mikehsm@outlook.com>
2022-04-17 02:50:20 +0300

    5.19: bloglist end to end testing, step3

--- cypress/integration/bloglist_app.spec.js
+++ cypress/integration/bloglist_app.spec.js
@@ -37,4 +37,39 @@ describe('Blog app', () => {
       cy.get('[data-cy=login-form]')
     })
   })
+
+  describe('When logged in', function () {
+    beforeEach(function () {
+      cy.request('POST', 'http://localhost:3003/api/login', {
+        username: 'admin',
+        password: 'admin',
+      }).then(({ body }) => {
+        localStorage.setItem('loggedUser', JSON.stringify(body))
+        cy.visit('http://localhost:3000')
+      })
+    })
+
+    it('A blog can be created', function () {
+      const blog = {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+      }
+
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:3003/api/blogs',
+        body: blog,
+        headers: {
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem('loggedUser')).token
+          }`,
+        },
+      })
+      cy.visit('http://localhost:3000')
+
+      cy.contains('React patterns')
+      cy.contains('Michael Chan')
+    })
+  })
 })
